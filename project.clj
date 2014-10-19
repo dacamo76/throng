5ae66acfbe0b7ef8cc952dce1ebc86b86ff02312
Daniel Alberto Canas <daniel_76@yahoo.com>
2014-10-18 19:02:44 -0600

    Begin next development iteration.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject dacamo76/throng "0.0.1"
+(defproject dacamo76/throng "0.0.2-SNAPSHOT"
   :description "A Clojure client library for LinkedIn's people API"
   :url "https://github.com/dacamo76/throng"
   :license {:name "Eclipse Public License"
